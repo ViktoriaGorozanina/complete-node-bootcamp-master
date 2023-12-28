afd068a03b70082af78afc090a7a9c49610909fb
Viktoria Gorozanina <gorozanina@gmail.com>
2023-12-28 22:06:01 +0200

    routing, API

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require(`fs`); //fs-file system
 const http = require(`http`); //creating http server
+const { json } = require("stream/consumers");
+const url = require(`url`); //creating http server
 
 /////////////////////////////////////
 
@@ -44,12 +46,86 @@ const http = require(`http`); //creating http server
 //SERVER
 
 //Create server and response
+
+const replaceTemplate = function (temp, product) {
+  //  /{%PRODUCTNAME%}/g means global so ALL these will be replacsed not only the 1st one.
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, `not-organic`);
+
+  return output;
+};
+
+//specify requests that should be done ONCE (will be loaded in the begining and then reused), SYNCHRONOUSLY:
+const tempOverview = fs.readFileSync(
+  `./1-node-farm/templates/template-overview.html`,
+  `utf-8`
+); //text file of html code of template-overview
+const tempCard = fs.readFileSync(
+  `./1-node-farm/templates/template-card.html`,
+  `utf-8`
+); //text file of html code of template-card
+const tempProduct = fs.readFileSync(
+  `./1-node-farm/templates/template-product.html`,
+  `utf-8`
+); //text file of html code of template-product page
+
+const data = fs.readFileSync(`./1-node-farm/dev-data/data.json`, `utf-8`);
+const dataObj = JSON.parse(data); //turns json file to object
+
+//////////////////////
+
+//specify stuff that will be called with every request, ASYNCHRONOUSLY:
 const server = http.createServer((request, resp) => {
-  console.log(request);
-  resp.end(`Hello from the server`);
+  const pathName = request.url;
+
+  //Building overview page:
+  if (pathName === `/` || pathName === `/overview`) {
+    resp.writeHead(200, { "Content-type": `text/html` });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join(``);
+    // console.log(cardsHtml);
+
+    const output = tempOverview.replace(`{%PRODUCT_CARDS%}`, cardsHtml);
+    resp.end(output);
+  }
+  //Building product page:
+  else if (pathName === `/product`) {
+    resp.writeHead(200, { "Content-type": `text/html` });
+    resp.end(tempProduct);
+  }
+  //API:
+  else if (pathName === `/api`) {
+    resp.writeHead(200, { "Content-type": `application/json` });
+    resp.end(data);
+  }
+  //NOT FOUND
+  else {
+    resp.writeHead(404, {
+      //header is sent BEFORE resposnse.end
+      "Content-type": `text/html`,
+      "my-own-header": `Hello hello`,
+    });
+    resp.end(`<h1>Page not found</h1>`);
+  }
 });
 
 //Listen to request
 server.listen(5500, `127.0.0.1`, () => {
   console.log(`start listening`);
 });
+
+//*-----------------------------------------Lesson 12
+
+//ROUTING
+
+//*-----------------------------------------Lesson 13
